a4c2d184633e2f1b76219817c7d7931702ee37ab
MaedaKouta <maedakoutaa@gmail.com>
2022-03-13 09:29:33 +0900

    Initial Commit

---
+++ MiniApp29-CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp29-CollectionView
+//
+//  Created by 前田航汰 on 2022/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
