1e3d7cd033b0a81f04c2f4c29aa51851e13f0d62
MaedaKouta <maedakoutaa@gmail.com>
2022-03-15 00:50:31 +0900

    commit

---
+++ MiniApp29-CollectionView/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  MiniApp29-CollectionView
+//
+//  Created by 前田航汰 on 2022/03/14.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var numberLabel: UILabel!
+}
--- MiniApp29-CollectionView/ViewController.swift
+++ MiniApp29-CollectionView/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        300
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.numberLabel.text = String(indexPath.row + 1)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let horaizenSpace:CGFloat = 20
+        let cellSizeWidth:CGFloat = self.view.frame.width/3 - horaizenSpace*2
+        return CGSize(width: cellSizeWidth, height: cellSizeWidth)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+      return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+    }
 
 }
-
